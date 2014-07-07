f2b35650b7210727581cc23fd0d2ec02a163db18
Zweer <flicofloc@gmail.com>
2014-07-07 21:40:12 +0200

    Updated less-middleware

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-module.exports = function(options){
+module.exports = function(){
   return function*(next){
-    yield less(this.req, this.res, options);
+    yield less(this.req, this.res, arguments);
     yield next;
   };
 };
 
 function less(req, res, options){
 	return function(callback){
-  	require('less-middleware')(options)(req, res, callback);
+  	require('less-middleware').apply(this, options)(req, res, callback);
 	};
 }
