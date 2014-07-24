7dd9bbc276b58f131555fb9c6e1090a542a988f6
Gustavo Salomé Silva <gustavonips@gmail.com>
2014-07-24 04:17:17 -0300

    fix error when not declared last

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = function(){
+	var options=arguments;
   return function*(next){
-    yield less(this.req, this.res, arguments);
+    yield less(this.req, this.res, options);
     yield next;
   };
 };
 
 function less(req, res, options){
-	return function(callback){
+  return function(callback){
   	require('less-middleware').apply(this, options)(req, res, callback);
 	};
 }
