284225eef8e77d8c78bd7ef6c4c2c23962c74c3c
Josef Sima <ja@chose.cz>
2014-07-24 09:43:05 +0200

    indentation fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = function(){
   var options=arguments;
   return function*(next){
-    	yield less(this.req, this.res, options);
-    	yield next;
+    yield less(this.req, this.res, options);
+    yield next;
   };
 };
 
 function less(req, res, options){
   return function(callback){
-  	require('less-middleware').apply(this, options)(req, res, callback);
+    require('less-middleware').apply(this, options)(req, res, callback);
   };
 }
